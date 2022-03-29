ba7b5756844c2e63d3b6e293c02fc1cf754443fd
wksadmin <wksadmin@DESKTOP-QUSV71Q>
2022-03-29 17:56:45 +0200

    update

--- Projet_Part_I/Transactions.cs
+++ Projet_Part_I/Transactions.cs
@@ -41,8 +41,9 @@ namespace Projet_Part_I
                     int id_trans = int.Parse(number);
 
                     string balance = column_values[1].Replace('.', ',');
-                    float solde = float.Parse(balance);
+                    float.TryParse(balance, out solde);
                     // remplacer le . par , pour eviter des bug dans le traitement en windows francais
+                    // float solde = float.TryParse(balance); ne gere pas les exceptions de solde vide
 
                     string sender = column_values[2];
                     //int expediteur = int.Parse(sender);
